5acabb957f2e623213c72d0d8d4d2c261454801f
randolj <r.recio@ufl.edu>
2023-11-12 16:30:15 -0500

    fixing code

--- Function.cs
+++ Function.cs
@@ -25,17 +25,18 @@ public class Function
         if (json.issue != null && json.issue.html_url != null)
         {
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        }
-        
+
         var client = new HttpClient();
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-        {
+            {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
+            };
 
         var response = client.Send(webRequest);
         using var reader = new StreamReader(response.Content.ReadAsStream());
             
         return reader.ReadToEnd();
+        }
+        return "fail";
     }
 }

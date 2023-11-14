d59d0aa7128afc8cc575d6a462cf9a0edea3bb2f
randolj <r.recio@ufl.edu>
2023-11-13 20:59:53 -0500

    fixing code

--- Function.cs
+++ Function.cs
@@ -12,46 +12,20 @@ namespace EsepWebhook
     {
         public string FunctionHandler(object input, ILambdaContext context)
         {
-            if (input == null)
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+            string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
+            var client = new HttpClient();
+            var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
             {
-                // Handle the case where input is null (if needed)
-                return "Input is null.";
-            }
-            try
-            {
-                dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+            };
 
-                // Check if the payload contains "issue" information
-                if (json.issue != null)
-                {
-                    // Extract the "html_url" from the "issue" information
-                    string issueHtmlUrl = json.issue.html_url;
-
-                    // Create a Slack message payload
-                    string payload = $"{{'text':'Issue Created: {issueHtmlUrl}'}}";
-
-                    // Post the payload to the Slack channel
-                    using var client = new HttpClient();
-                    using var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
-                    {
-                        Content = new StringContent(payload, Encoding.UTF8, "application/json")
-                    };
-
-                    var response = client.Send(webRequest);
-
-                    // Read and return the response from the Slack API
-                    using var reader = new StreamReader(response.Content.ReadAsStream());
-                    return reader.ReadToEnd();
-                }
-
-                // If the payload doesn't contain "issue" information, return a message indicating that
-                return "No issue information found in the payload.";
-            }
-            catch (Exception ex)
-            {
-                // Handle exceptions and return an error message
-                return $"Error: {ex.Message}";
-            }
+            var response = client.Send(webRequest);
+            using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+            return reader.ReadToEnd();
         }
     }
 }
